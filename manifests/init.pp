90237179ffbdf28c6b1792cc57c61030a900fdc5
Adrien Thebo <git@somethingsinistral.net>
2013-01-11 12:54:09 -0800

    add rack inclusion as unicorn dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@ class unicorn($provider = 'gem', $ensure = 'present') {
 
   # The unicorn gem has prerequisites that requires building native extensions.
   require ruby::dev
+  include rack
 
   package { 'unicorn':
     ensure   => $ensure,
