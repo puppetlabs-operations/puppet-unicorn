1f15bb1689f99e74f28077a3dda806423f937859
Zach Leslie <zach@puppetlabs.com>
2014-02-08 09:26:41 -0800

    Begin adding support for FreeBSD

--- manifests/app.pp
+++ manifests/app.pp
@@ -5,10 +5,10 @@ define unicorn::app (
   $backlog         = '2048',
   $workers         = $::processorcount,
   $user            = 'root',
-  $group           = 'root',
+  $group           = '0',
   $config_file     = '',
   $config_template = 'unicorn/config_unicorn.config.rb.erb',
-  $initscript      = "unicorn/init-unicorn.erb",
+  $initscript      = 'unicorn/init-unicorn.erb',
   $logdir          = "${approot}/log",
   $rack_env        = 'production',
   $preload_app     = false,
@@ -17,6 +17,12 @@ define unicorn::app (
 
   require unicorn
   include unicorn::params
+
+  $rc_d = $unicorn::params::rc_d
+
+  if ! $rc_d {
+    fail('unicorn is not supported on this platform')
+  }
 
   # If we have been given a config path, use it, if not, make one up.
   # This _may_ not be the most secure, as it should live outside of
@@ -48,33 +54,36 @@ define unicorn::app (
     ensure     => running,
     enable     => true,
     hasstatus  => true,
-    start      => "/etc/init.d/unicorn_${name} start",
-    stop       => "/etc/init.d/unicorn_${name} stop",
-    restart    => "/etc/init.d/unicorn_${name} reload",
+    start      => "${rc_d}/unicorn_${name} start",
+    stop       => "${rc_d}/unicorn_${name} stop",
+    restart    => "${rc_d}/unicorn_${name} reload",
     require    => [
-      File["/etc/init.d/unicorn_${name}"],
+      File["${rc_d}/unicorn_${name}"],
       File["/etc/default/unicorn_${name}"],
     ],
   }
 
-  file {
-    "/etc/default/unicorn_${name}":
-      owner   => root,
-      group   => root,
-      mode    => 644,
-      content => template("unicorn/default-unicorn.erb"),
-      notify  => Service["unicorn_${name}"];
-    "/etc/init.d/unicorn_${name}":
-      owner   => root,
-      group   => root,
-      mode    => 755,
-      content => template($initscript),
-      notify  => Service["unicorn_${name}"];
-    $config:
-      owner   => root,
-      group   => root,
-      mode    => 644,
-      content => template($config_template),
-      notify  => Service["unicorn_${name}"];
+  file { "/etc/default/unicorn_${name}":
+    owner   => 'root',
+    group   => '0',
+    mode    => '0644',
+    content => template('unicorn/default-unicorn.erb'),
+    notify  => Service["unicorn_${name}"],
+  }
+
+  file { "${rc_d}/unicorn_${name}":
+    owner   => 'root',
+    group   => '0',
+    mode    => '0755',
+    content => template($initscript),
+    notify  => Service["unicorn_${name}"],
+  }
+
+  file { $config:
+    owner   => 'root',
+    group   => '0',
+    mode    => '0644',
+    content => template($config_template),
+    notify  => Service["unicorn_${name}"];
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,30 @@
 class unicorn::params {
 
-  case $lsbmajdistrelease {
-    '6': {
-      $unicorn_executable = "/var/lib/gems/1.8/bin/unicorn"
-      $bundler_executable = "/var/lib/gems/1.8/bin/bundle"
+  case $::operatingsystem {
+    'debian': {
+      case $::lsbmajdistrelease {
+        '6': {
+          $unicorn_executable = '/var/lib/gems/1.8/bin/unicorn'
+          $bundler_executable = '/var/lib/gems/1.8/bin/bundle'
+        }
+        default: {
+          $unicorn_executable = '/usr/local/bin/unicorn'
+          $bundler_executable = '/usr/local/bin/bundle'
+        }
+      }
     }
-    default: {
-      $unicorn_executable = "/usr/local/bin/unicorn"
-      $bundler_executable = "/usr/local/bin/bundle"
+    'freebsd': {
+      $unicorn_executable = '/usr/local/bin/unicorn'
+      $bundler_executable = '/usr/local/bin/bundle'
+    }
+  }
+
+  case $::kernel {
+    'linux': {
+      $rc_d = '/etc/init.d'
+    }
+    'freebsd': {
+      $rc_d = '/usr/local/etc/rc.d'
     }
   }
 }
