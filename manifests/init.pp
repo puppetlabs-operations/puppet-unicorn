0ceb45d30935803c9fc5c8e8d6100adc906b3ae3
Daniel Parks <dp@puppetlabs.com>
2016-08-06 15:47:12 -0700

    Option to not manage package, ::rack, and ::ruby::dev

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,17 @@
-class unicorn(
+class unicorn (
   $export_home = '',
+  $manage_package = true,
   $ensure   = 'present',
   $provider = 'gem',
 ) {
+  if $manage_package {
+    # The unicorn gem has prerequisites that requires building native extensions.
+    require ::ruby::dev
+    include ::rack
 
-  # The unicorn gem has prerequisites that requires building native extensions.
-  require ruby::dev
-  include rack
-
-  package { 'unicorn':
-    ensure   => $ensure,
-    provider => $provider,
+    package { 'unicorn':
+      ensure   => $ensure,
+      provider => $provider,
+    }
   }
 }
